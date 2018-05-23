35873b4844319d1dfcaf3c8cef1f377d83c6e9cc
Andreas M. Brunnet <andreas.brunnet@gmail.com>
2018-05-23 20:04:03 +0200

    Camera takes a sample picture and displays number of bytes in application

--- app/src/main/java/ais/hskl/tobi/MainActivity.kt
+++ app/src/main/java/ais/hskl/tobi/MainActivity.kt
@@ -1,10 +1,16 @@
 package ais.hskl.tobi
 
-import android.content.Context
-import android.hardware.camera2.CameraManager
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.hardware.Camera
+import android.hardware.Camera.CameraInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.SurfaceView
+import android.widget.TextView
+import android.widget.Toast
 
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface
 import java.nio.ByteBuffer
@@ -16,18 +22,123 @@ class MainActivity : AppCompatActivity() {
     private var INPUT_NODE = "image_tensor"
     private var OUTPUT_NODES = arrayOf("num_detections", "detection_boxes", "detection_scores", "detection_classes")
 
+    private var camera: Camera? = null
+    private var cameraId = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var cameraManager = getSystemService(Context.CAMERA_SERVICE)
-        if(cameraManager is CameraManager){
-            var cameraIds = cameraManager.cameraIdList
-            var characteristics = cameraManager.getCameraCharacteristics(cameraIds[0])
+
+        if(!packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+
+            Toast.makeText(this, "Could not find any camera device", Toast.LENGTH_LONG).show()
+
+        }else{
+
+            this.cameraId = findPrimaryCamera()
+
+            if(this.cameraId < 0){
+
+                Toast.makeText(this, "Could not find any primary camera", Toast.LENGTH_LONG).show()
+
+            }
         }
 
-        System.loadLibrary("tensorflow_inference")
+        //System.loadLibrary("tensorflow_inference")
 
-        initializeTensorFlow()
+        //initializeTensorFlow()
+    }
+
+    var pictureCallback: Camera.PictureCallback = object: Camera.PictureCallback{
+        override fun onPictureTaken(byteData: ByteArray?, cameraObject: Camera?) {
+
+            findViewById<TextView>(R.id.testText).text = byteData?.size.toString()
+
+            if(byteData != null){
+
+                var bitmap = BitmapFactory.decodeByteArray(byteData, 0, byteData.size)
+
+                    var data:IntArray = IntArray(bitmap.width * bitmap.height)
+                    bitmap.getPixels(data,0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+                    Log.i("MainActivity", "Data: " + data[10])
+            }
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if(safeCameraOpen(this.cameraId)) {
+
+            //Initialize fake SurfaceView
+            var surfaceView = SurfaceView(this)
+
+            try {
+
+                this.camera?.setPreviewDisplay(surfaceView.holder)
+
+            } catch (e: Exception) {
+
+                Log.e("MainActivity", "Exception while setting preview display surface")
+
+            }
+
+            this.camera?.startPreview()
+
+            // camera.parameters
+
+            this.camera?.takePicture(null, null, pictureCallback)
+        }
+    }
+
+    public override fun onPause() {
+        releaseCamera()
+        super.onPause()
+    }
+
+    private fun safeCameraOpen(id: Int): Boolean{
+
+        var isOpen: Boolean = false
+
+        try{
+            releaseCamera()
+            this.camera = Camera.open(id)
+
+            if(this.camera != null)
+                isOpen = true
+        }catch(e: Exception){
+
+            Log.e("MainActivity", "Could not safely open camera")
+            Log.e("MainActivity", e.message)
+
+        }
+
+        return isOpen
+    }
+
+    private fun releaseCamera(){
+
+            this.camera?.stopPreview()
+            this.camera?.release()
+
+    }
+
+    private fun findPrimaryCamera(): Int {
+        var cameraId = -1
+
+
+        for (index in 0..Camera.getNumberOfCameras()){
+            var info = CameraInfo()
+            Camera.getCameraInfo(index, info)
+
+            if(info.facing == CameraInfo.CAMERA_FACING_BACK){
+                cameraId = index
+                break
+            }
+        }
+
+        return cameraId
     }
 
     private fun initializeTensorFlow() {
