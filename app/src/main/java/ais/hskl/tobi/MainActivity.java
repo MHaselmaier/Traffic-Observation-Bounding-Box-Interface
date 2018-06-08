700524b05ded27ffc8967ad6491741dfca20fecb
Andreas M. Brunnet <andreas.brunnet@gmail.com>
2018-06-08 17:54:25 +0200

    Added permissions for GPS to manifest. Added basic code for location

--- app/src/main/java/ais/hskl/tobi/MainActivity.java
+++ app/src/main/java/ais/hskl/tobi/MainActivity.java
@@ -2,7 +2,11 @@ package ais.hskl.tobi;
 
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +23,34 @@ public class MainActivity extends AppCompatActivity {
      protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener listener = new LocationListener(){
+
+            @Override
+            public void onLocationChanged(Location location) {
+                Toast.makeText(MainActivity.this, "Speed: " + location.getSpeed(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+                0, listener);
     }
 
     @Override
