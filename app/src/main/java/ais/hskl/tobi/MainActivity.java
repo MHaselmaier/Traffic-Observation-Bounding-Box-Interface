686b8d0bb0e56b35859a77a1f261472e26fd002d
Antonino Provenzano <provenzano.a@gmx.net>
2018-06-24 17:23:51 +0200

    Tobi confidence score is now saved in shared preferences.

--- app/src/main/java/ais/hskl/tobi/MainActivity.java
+++ app/src/main/java/ais/hskl/tobi/MainActivity.java
@@ -6,11 +6,11 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Switch;
 import android.widget.Toast;
+import android.content.SharedPreferences;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity
     private static final int CAMERA_PERMISSION_CODE = 42;
     private BoundingBoxView boundingBoxView;
     private Switch showDebugInfo;
+    private static final String FILENAME = "dataFile";
+    private static final String VAL_KEY = "key_detectionScore";
 
     private TobiNetwork tobi;
 
@@ -41,7 +43,6 @@ public class MainActivity extends AppCompatActivity
                 MainActivity.this.boundingBoxView.showDebugInfo(MainActivity.this.showDebugInfo.isChecked()));
 
         Button minDetectionScore = findViewById(R.id.min_detection_score);
-        minDetectionScore.setText(getResources().getString(R.string.min_detection_score, (int)(this.tobi.getMinDetectionScore() * 100)));
         MinDetectionScoreDialog minDetectionScoreDialog = new MinDetectionScoreDialog(this, minDetectionScore, this.tobi);
         minDetectionScore.setOnClickListener((v) -> minDetectionScoreDialog.show());
     }
@@ -53,11 +54,19 @@ public class MainActivity extends AppCompatActivity
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
         }
+
+        this.tobi.setMinDetectionScore(getSharedPreferences(FILENAME, MODE_PRIVATE).getFloat(VAL_KEY, 0.7f));
+        Button minDetectionScore = findViewById(R.id.min_detection_score);
+        minDetectionScore.setText(getResources().getString(R.string.min_detection_score, (int)(this.tobi.getMinDetectionScore() * 100)));
     }
 
     @Override
     protected void onPause(){
         //Doing stuff when pausing the application before actually calling the super method... otherwise would be stupid
+        SharedPreferences sharedPrefs = getSharedPreferences(FILENAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putFloat(VAL_KEY, this.tobi.getMinDetectionScore());
+        editor.commit();
 
         super.onPause();
     }
